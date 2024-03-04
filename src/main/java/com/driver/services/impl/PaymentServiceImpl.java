8c73ab05f91f741a53481239c4cfb0ce647eb045
rohitnitjsr1801 <rohitrajrk1801@gmail.com>
2024-03-04 13:04:37 +0530

    seventh commit

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,13 +1,13 @@
 package com.driver.services.impl;
 
-import com.driver.model.*;
+import com.driver.model.Payment;
+import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -18,10 +18,15 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
-        Optional<Reservation> r1=reservationRepository2.findById(reservationId);
-        if(r1.isEmpty())
-        {
-            throw new RuntimeException("Reservation ID invalid");
+        Reservation reservation;
+        try{
+            reservation = reservationRepository2.findById(reservationId).get();
+        }catch (Exception e){
+            throw new Exception("Reservation id not valid");
+        }
+
+        if(amountSent < reservation.getSpot().getPricePerHour() * reservation.getNumberOfHours()){
+            throw new Exception("Insufficient Amount");
         }
         PaymentMode paymentMode = null;
         if (mode.toUpperCase().equals(PaymentMode.CASH.toString())) paymentMode = PaymentMode.CASH;
@@ -32,21 +37,15 @@ public class PaymentServiceImpl implements PaymentService {
         }else {
             throw new Exception("Payment mode not detected");
         }
-        Reservation reservation=r1.get();
-        Spot spot=reservation.getSpot();
 
-        int payable=reservation.getNumberOfHours()*spot.getPricePerHour();
-        if(payable>amountSent)
-        {
-            throw new RuntimeException("Insufficient Amount");
-        }
-        Payment payment=new Payment();
+        Payment payment = new Payment();
         payment.setPaymentCompleted(true);
         payment.setPaymentMode(paymentMode);
         payment.setReservation(reservation);
-        paymentRepository2.save(payment);
-        reservation.setPayment(payment);
+
+//        paymentRepository2.save(payment);
         reservationRepository2.save(reservation);
-        return payment;
+        return  payment;
+
     }
 }
